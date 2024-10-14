35c92792a00a982da90daf8bf9bf4facfc910e6f
SamuelFoc <samo.sipikal@gmail.com>
2024-10-14 16:42:04 +0200

    Add TS configuration

---
+++ 3 - TS config/src/index.ts
@@ -0,0 +1,7 @@
+console.log("Hello, TS config!");
+
+let i: number = 0;
+
+// In tsconfig.json set up the rootDir to be ./src and outDir to be ./dist
+// (you can set the path as you want) so it will automatically look in root dir
+// when compiling the code and save it to the outDir
