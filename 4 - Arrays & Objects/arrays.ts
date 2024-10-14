44374844129c275c13a67cffe8cf4a0ad1308eef
SamuelFoc <samo.sipikal@gmail.com>
2024-10-14 16:42:15 +0200

    Add arrays and objects

---
+++ 4 - Arrays & Objects/arrays.ts
@@ -0,0 +1,8 @@
+// Define an array of strings
+let names: string[] = ["a", "b", "c"];
+
+// Define an array of numbers
+let ages: number[] = [1, 2, 3];
+
+// TS also supports infering of types (automatically set to string[])
+let fruits = ["apple", "banana", "mango"];
---
+++ 4 - Arrays & Objects/objects.ts
@@ -0,0 +1,12 @@
+// Defining an object with specific types for its properties
+let user: { firstName: string; age: number; id: number } = {
+  firstName: "Mario",
+  age: 26,
+  id: 1,
+};
+
+// Type inference with objects
+let person = {
+  name: "George",
+  score: 100,
+};
